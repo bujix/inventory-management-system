f47f44beb5a31e991cdaa35077f5276a02047940
bujix <98456074+bujix@users.noreply.github.com>
2026-01-06 21:00:13 +0300

    Add Order model class

---
+++ src/inventory/model/Order.java
@@ -0,0 +1,45 @@
+package inventory.model;
+
+import java.time.LocalDate;
+
+/**
+ * Order sınıfı, sistemde oluşturulan bir siparişi temsil eder.
+ * Bir ürün, sipariş miktarı ve sipariş tarihi bilgilerini içerir.
+ */
+public class Order {
+
+    private String orderId;
+    private Product product;
+    private int quantity;
+    private LocalDate orderDate;
+
+    /**
+     * Yeni bir sipariş oluşturur.
+     *
+     * @param orderId siparişin benzersiz kimliği
+     * @param product sipariş edilen ürün
+     * @param quantity sipariş edilen miktar
+     */
+    public Order(String orderId, Product product, int quantity) {
+        this.orderId = orderId;
+        this.product = product;
+        this.quantity = quantity;
+        this.orderDate = LocalDate.now();
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+}
